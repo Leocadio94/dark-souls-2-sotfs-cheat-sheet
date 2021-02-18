a4df13939ec1bd79c4212d36b290facf0c24e44e
Gabriel Leocadio <gabrielsilva94@gmail.com>
2021-02-17 21:54:49 -0300

    Add Bootstrap Themes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+var profilesKey = 'ds2_profiles';
+
 (function($) {
+
+    var themes = {
+        "Standard" : "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+        "Cosmo" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css",
+        "Cyborg" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cyborg/bootstrap.min.css",
+        "Darkly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/darkly/bootstrap.min.css",
+        "Flatly" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css",
+        "Journal" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/journal/bootstrap.min.css",
+        "Lumen" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/lumen/bootstrap.min.css",
+        "Paper" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/paper/bootstrap.min.css",
+        "Readable" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/readable/bootstrap.min.css",
+        "Sandstone" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/sandstone/bootstrap.min.css",
+        "Simplex" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css",
+        "Slate" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/slate/bootstrap.min.css",
+        "Spacelab" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/spacelab/bootstrap.min.css",
+        "Superhero" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/superhero/bootstrap.min.css",
+        "United" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/united/bootstrap.min.css",
+        "Yeti" : "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/yeti/bootstrap.min.css"
+    };
 
     var defaultProfiles = {
         'current': 'Default Profile'
@@ -11,6 +32,8 @@
     var profiles = $.jStorage.get(profilesKey, defaultProfiles);
 
     jQuery(document).ready(function($) {
+        // Get the right style going...
+        themeSetup(buildThemeSelection());
 
         // TODO Find a better way to do this in one pass
         $('ul li[data-id]').each(function() {
@@ -35,6 +58,13 @@
             });
             $.jStorage.set(profilesKey, profiles);
             calculateTotals();
+        });
+
+        // Theme callback
+        $('#themes').change(function(event) {
+            var stylesheet = $('#themes').val();
+            themeSetup(stylesheet);
+            $.jStorage.set("style", stylesheet);
         });
 
         $('#profiles').change(function(event) {
@@ -165,6 +195,26 @@
         calculateTotals();
 
     });
+
+    // Setup ("bootstrap", haha) styling
+    function themeSetup(stylesheet) {
+        if(stylesheet === null || stylesheet === undefined) { // if we didn't get a param, then
+            stylesheet = $.jStorage.get("style") || "Standard"; // fall back on "light" if cookie not set
+        }
+        $("#bootstrap").attr("href", themes[stylesheet]);
+    }
+
+    function buildThemeSelection() {
+        var style = $.jStorage.get("style") || "Standard";
+        var themeSelect = $("#themes");
+        $.each(themes, function(key, value){
+            themeSelect.append(
+                $('<option></option>').val(key).html(key + " Theme")
+            );
+        });
+        themeSelect.val(style);
+        return style;
+    }
 
     function populateProfiles() {
         $('#profiles').empty();
